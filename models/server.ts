3956b48954862b7cc248e8c81e1a6900089aaf32
unknown <matuuzg@gmail.com>
2024-01-13 13:18:39 -0500

    modifique archivos

--- models/server.ts
+++ models/server.ts
@@ -1,39 +1,33 @@
-import express, {Express} from 'express';
-
+import express, { Express } from "express";
 import cors from 'cors';
+import { dbConnection } from "../database/config";
 
-import { dbConnection } from '../database/config';
-
-import authRoutes from '../routes/auth';
-
-import ordersRoutes from '../routes/orders';
-
+import authRoutes from "../routes/auth"
+import ordersRoutes from "../routes/orders";
+import issuesRoutes  from "../routes/issues";
 
 export class Server {
- app: Express;
- port: string | number | undefined;
- authPath: string;
- ordersPath: string;
+    app: Express;
+    port: string | number | undefined;
+    authPath: string;
+    ordersPath: string;
+    issuesPath: string;
 
-    constructor() {
-    this.app = express();
-    this.port = process.env.PORT
-    this.authPath = '/auth';
-    this.ordersPath = '/orders';
+    constructor(){
+        this.app= express();
+        this.port= process.env.PORT
+        this.authPath = '/auth'
+        this.ordersPath = '/orders'
+        this.issuesPath = '/issues';
 
-    // Connect to database
-    this.connectDB();
+        this.conectarDB();
 
-    this.middlewares();
+        this.middlewares();
 
-    // Routes
-
-    this.routes();
-
-
+        this.routes()
     }
 
-    async connectDB(): Promise<void> {
+    async conectarDB(): Promise<void> {
         await dbConnection();
     }
 
@@ -42,14 +36,17 @@ export class Server {
         this.app.use(express.json());
     }
 
-    routes(): void {
+
+    routes():void {
         this.app.use(this.authPath, authRoutes);
-        this.app.use(this.ordersPath, ordersRoutes);
+        this.app.use(this.ordersPath, ordersRoutes)
+        this.app.use(this.issuesPath, issuesRoutes);
     }
 
     listen(): void {
-        this.app.listen(this.port, () => {
-            console.log(`Server running on port ${this.port}`);
-        });
+        this.app.listen(this.port, ()=>{
+            console.log(`Corriendo en el puerto ${this.port}`)
+        })
     }
+
 }
